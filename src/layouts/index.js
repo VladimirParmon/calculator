b2235cd45c190e5daea1d334448de108190d2312
VladimirParmon <parmon_vladimir@bk.ru>
2022-07-12 10:36:49 +0300

    remove some files and conf. prettier

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,9 +3,5 @@ import styled from 'styled-components';
 export const PageLayout = styled.div`
   width: 100%;
   height: 100%;
-
-  max-width: 1280px;
-
-  padding: ${({ theme }) => theme.spaces[4]}px;
   margin: 0 auto;
-`
+`;
